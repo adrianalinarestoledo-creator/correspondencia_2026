d5aa29a7f4e84564976ed71777a41a3ecd50f11f
adrianalinarestoledo-creator <adrianalinarestoledo@gmail.com>
2026-05-11 12:36:34 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, send_file
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timedelta
 from werkzeug.security import generate_password_hash, check_password_hash
